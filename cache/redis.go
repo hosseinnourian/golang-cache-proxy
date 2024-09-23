674db6ca83e89f4fe71d96d16cb9083c36d2b6ec
hosseinnourian <hosseinnourian@protonmail.com>
2024-09-23 19:30:20 +0330

    add redis

---
+++ cache/redis.go
@@ -0,0 +1,20 @@
+package cache
+
+type Redis struct {
+}
+
+func RedisConnection() {
+
+}
+
+func NewRedis() {
+
+}
+
+func (r *Redis) ReadFromCache() {
+
+}
+
+func (r *Redis) WriteToCache() {
+
+}
